a8207445c5a55caecc9b165963fa0e1eeb2aa314
John West <jowest1990@gmail.com>
2022-03-30 14:28:59 -0500

    Scala Home work done
    completed all the challenges For Scala

--- scala-hello-world/src/main/scala/com/labs1904/hwe/App.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/App.scala
@@ -1,8 +1,12 @@
 package com.labs1904.hwe
 
 object App {
+
+  val name: String = "John"
+
   def main(args: Array[String]): Unit = {
-    println("Hello World")
+//    println("Hello World")
+    greeting(name)
   }
 
   def greeting(name: String): String = {
--- scala-hello-world/src/main/scala/com/labs1904/hwe/practice/OptionEither.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/practice/OptionEither.scala
@@ -6,21 +6,47 @@ case class WeatherStation(name: String, temperature: Option[Int])
 
 object OptionEither {
   /*
-    Returns age of a dog when given a human age.
-    Returns None if the input is None.
-  */
-  def dogAge(humanAge: Option[Int]): Option[Int] = ???
+   Returns age of a dog when given a human age.
+   Returns None if the input is None.
+ */
+  def dogAge(humanAge: Option[Int]): Option[Int] = {
+    if (humanAge.isEmpty) {
+      None
+    } else {
+      humanAge.map(_ * 7)
+    }
+  }
 
   /*
     Returns the total cost af any item.
     If that item has a price, then the price + 7% of the price should be returned.
   */
-  def totalCost(item: Item): Option[Double] = ???
+  def totalCost(item: Item): Option[Double] = {
+    item.price match {
+      case Some(price) => Option(price + (price * .07))
+      case None => None
+    }
+  }
 
   /*
     Given a list of weather temperatures, calculates the average temperature across all weather stations.
     Some weather stations don't report temperature
     Returns None if the list is empty or no weather stations contain any temperature reading.
    */
-  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = ???
+  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = {
+    var sum = 0
+    var numTemps = 0
+    for (weatherStation <- temperatures) {
+      if (weatherStation.temperature.isDefined) {
+        val temp = weatherStation.temperature.get
+        sum += temp
+        numTemps += 1
+      }
+    }
+    if (numTemps > 0) {
+      Some(sum / numTemps)
+    } else {
+      None
+    }
+  }
 }
