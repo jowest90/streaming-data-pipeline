39306936beba4ba7da732094b7b0c178dff35290
John West <jowest1990@gmail.com>
2022-04-05 10:30:23 -0500

    show output
    showing the output of the lab work

--- kafka-hello-world/src/main/scala/com/labs1904/hwe/consumers/HweConsumer.scala
+++ kafka-hello-world/src/main/scala/com/labs1904/hwe/consumers/HweConsumer.scala
@@ -50,6 +50,29 @@ object HweConsumer {
         // Retrieve the message from each record
         val message = record.value()
         println(s"Message Received: $message")
+        /*
+        OUTPUT:
+Message Received: 1,Deanna Kihn,kihndeanna@yahoo.com
+Message Received: 5,Bartholome Spinka IV,iv_spinka_bartholome@yahoo.com
+Message Received: 7,Afton Goyette,goyette.afton@hotmail.com
+Message Received: 8,Edwin Lind,lind.edwin@yahoo.com
+Message Received: 1,Alyson Kessler IV,kessleralysoniv@gmail.com
+Message Received: 5,Miss Cortney Labadie,cortney_labadie_miss@hotmail.com
+Message Received: 7,Giovani Wisozk,giovani.wisozk@hotmail.com
+Message Received: 8,Roel Hintz,hintz.roel@yahoo.com
+Message Received: 4,Vicky Wunsch II,wunschiivicky@yahoo.com
+Message Received: 6,Flavio Klocko,flavioklocko@hotmail.com
+Message Received: 10,Matilde Sporer,sporermatilde@yahoo.com
+Message Received: 4,Frances Harris,frances_harris@yahoo.com
+Message Received: 6,Morton Monahan,monahan.morton@hotmail.com
+Message Received: 10,Karli Weimann,karli_weimann@gmail.com
+Message Received: 2,Abelardo O'Keefe,abelardoo'keefe@hotmail.com
+Message Received: 3,Enrique Kerluke,kerluke.enrique@yahoo.com
+Message Received: 9,Kiley Will DDS,will_dds_kiley@hotmail.com
+Message Received: 2,Jena Koch,koch_jena@gmail.com
+Message Received: 3,Gail Keeling,gailkeeling@yahoo.com
+Message Received: 9,Gerson Shields,gerson.shields@hotmail.com
+         */
         // TODO: Add business logic here!
         //split lines by comma
         val user = message.split(",").map(_.trim)
@@ -61,11 +84,108 @@ object HweConsumer {
         val email = user(2)
         //RawUser
         val rawUserLog = new RawUser(id,name,email)
-        println(rawUserLog)
+//        println(s"Raw User Received: "+rawUserLog.id+ " "+rawUserLog.name+ " "+rawUserLog.email)
+        /*
+        OUTPUT:
+Raw User Received: 4 Vicky Wunsch II wunschiivicky@yahoo.com
+Raw User Received: 6 Flavio Klocko flavioklocko@hotmail.com
+Raw User Received: 10 Matilde Sporer sporermatilde@yahoo.com
+Raw User Received: 4 Frances Harris frances_harris@yahoo.com
+Raw User Received: 6 Morton Monahan monahan.morton@hotmail.com
+Raw User Received: 10 Karli Weimann karli_weimann@gmail.com
+Raw User Received: 1 Deanna Kihn kihndeanna@yahoo.com
+Raw User Received: 5 Bartholome Spinka IV iv_spinka_bartholome@yahoo.com
+Raw User Received: 7 Afton Goyette goyette.afton@hotmail.com
+Raw User Received: 8 Edwin Lind lind.edwin@yahoo.com
+Raw User Received: 1 Alyson Kessler IV kessleralysoniv@gmail.com
+Raw User Received: 5 Miss Cortney Labadie cortney_labadie_miss@hotmail.com
+Raw User Received: 7 Giovani Wisozk giovani.wisozk@hotmail.com
+Raw User Received: 8 Roel Hintz hintz.roel@yahoo.com
+Raw User Received: 2 Abelardo O'Keefe abelardoo'keefe@hotmail.com
+Raw User Received: 3 Enrique Kerluke kerluke.enrique@yahoo.com
+Raw User Received: 9 Kiley Will DDS will_dds_kiley@hotmail.com
+Raw User Received: 2 Jena Koch koch_jena@gmail.com
+Raw User Received: 3 Gail Keeling gailkeeling@yahoo.com
+Raw User Received: 9 Gerson Shields gerson.shields@hotmail.com
+         */
+//        println(s"Raw User Received: "+rawUserLog)
+        /*
+        OUTPUT:
+Raw User Received: RawUser(4,Vicky Wunsch II,wunschiivicky@yahoo.com)
+Raw User Received: RawUser(6,Flavio Klocko,flavioklocko@hotmail.com)
+Raw User Received: RawUser(10,Matilde Sporer,sporermatilde@yahoo.com)
+Raw User Received: RawUser(4,Frances Harris,frances_harris@yahoo.com)
+Raw User Received: RawUser(6,Morton Monahan,monahan.morton@hotmail.com)
+Raw User Received: RawUser(10,Karli Weimann,karli_weimann@gmail.com)
+Raw User Received: RawUser(1,Deanna Kihn,kihndeanna@yahoo.com)
+Raw User Received: RawUser(5,Bartholome Spinka IV,iv_spinka_bartholome@yahoo.com)
+Raw User Received: RawUser(7,Afton Goyette,goyette.afton@hotmail.com)
+Raw User Received: RawUser(8,Edwin Lind,lind.edwin@yahoo.com)
+Raw User Received: RawUser(1,Alyson Kessler IV,kessleralysoniv@gmail.com)
+Raw User Received: RawUser(5,Miss Cortney Labadie,cortney_labadie_miss@hotmail.com)
+Raw User Received: RawUser(7,Giovani Wisozk,giovani.wisozk@hotmail.com)
+Raw User Received: RawUser(8,Roel Hintz,hintz.roel@yahoo.com)
+Raw User Received: RawUser(2,Abelardo O'Keefe,abelardoo'keefe@hotmail.com)
+Raw User Received: RawUser(3,Enrique Kerluke,kerluke.enrique@yahoo.com)
+Raw User Received: RawUser(9,Kiley Will DDS,will_dds_kiley@hotmail.com)
+Raw User Received: RawUser(2,Jena Koch,koch_jena@gmail.com)
+Raw User Received: RawUser(3,Gail Keeling,gailkeeling@yahoo.com)
+Raw User Received: RawUser(9,Gerson Shields,gerson.shields@hotmail.com)
+         */
+//        println(rawUserLog)
+
 
         //EnrichedUser
         val enrichedUserLog = new EnrichedUser(id,numberAsWord,name,hweDeveloper,email)
-        println(enrichedUserLog)
+//        println(s"Enriched User Received: "+enrichedUserLog.id+ " "+enrichedUserLog.numberAsWord+ " "+enrichedUserLog.name+ " "+enrichedUserLog.hweDeveloper+ " "+enrichedUserLog.email)
+        /*
+        OUTPUT:
+Enriched User Received: 1 1 Deanna Kihn John West kihndeanna@yahoo.com
+Enriched User Received: 5 5 Bartholome Spinka IV John West iv_spinka_bartholome@yahoo.com
+Enriched User Received: 7 7 Afton Goyette John West goyette.afton@hotmail.com
+Enriched User Received: 8 8 Edwin Lind John West lind.edwin@yahoo.com
+Enriched User Received: 1 1 Alyson Kessler IV John West kessleralysoniv@gmail.com
+Enriched User Received: 5 5 Miss Cortney Labadie John West cortney_labadie_miss@hotmail.com
+Enriched User Received: 7 7 Giovani Wisozk John West giovani.wisozk@hotmail.com
+Enriched User Received: 8 8 Roel Hintz John West hintz.roel@yahoo.com
+Enriched User Received: 4 4 Vicky Wunsch II John West wunschiivicky@yahoo.com
+Enriched User Received: 6 6 Flavio Klocko John West flavioklocko@hotmail.com
+Enriched User Received: 10 10 Matilde Sporer John West sporermatilde@yahoo.com
+Enriched User Received: 4 4 Frances Harris John West frances_harris@yahoo.com
+Enriched User Received: 6 6 Morton Monahan John West monahan.morton@hotmail.com
+Enriched User Received: 10 10 Karli Weimann John West karli_weimann@gmail.com
+Enriched User Received: 2 2 Abelardo O'Keefe John West abelardoo'keefe@hotmail.com
+Enriched User Received: 3 3 Enrique Kerluke John West kerluke.enrique@yahoo.com
+Enriched User Received: 9 9 Kiley Will DDS John West will_dds_kiley@hotmail.com
+Enriched User Received: 2 2 Jena Koch John West koch_jena@gmail.com
+Enriched User Received: 3 3 Gail Keeling John West gailkeeling@yahoo.com
+Enriched User Received: 9 9 Gerson Shields John West gerson.shields@hotmail.com
+         */
+//        println(s"Enriched User Received: "+enrichedUserLog)
+        /*
+        OUTPUT:
+Enriched User Received: EnrichedUser(2,2,Abelardo O'Keefe,John West,abelardoo'keefe@hotmail.com)
+Enriched User Received: EnrichedUser(3,3,Enrique Kerluke,John West,kerluke.enrique@yahoo.com)
+Enriched User Received: EnrichedUser(9,9,Kiley Will DDS,John West,will_dds_kiley@hotmail.com)
+Enriched User Received: EnrichedUser(2,2,Jena Koch,John West,koch_jena@gmail.com)
+Enriched User Received: EnrichedUser(3,3,Gail Keeling,John West,gailkeeling@yahoo.com)
+Enriched User Received: EnrichedUser(9,9,Gerson Shields,John West,gerson.shields@hotmail.com)
+Enriched User Received: EnrichedUser(4,4,Vicky Wunsch II,John West,wunschiivicky@yahoo.com)
+Enriched User Received: EnrichedUser(6,6,Flavio Klocko,John West,flavioklocko@hotmail.com)
+Enriched User Received: EnrichedUser(10,10,Matilde Sporer,John West,sporermatilde@yahoo.com)
+Enriched User Received: EnrichedUser(4,4,Frances Harris,John West,frances_harris@yahoo.com)
+Enriched User Received: EnrichedUser(6,6,Morton Monahan,John West,monahan.morton@hotmail.com)
+Enriched User Received: EnrichedUser(10,10,Karli Weimann,John West,karli_weimann@gmail.com)
+Enriched User Received: EnrichedUser(1,1,Deanna Kihn,John West,kihndeanna@yahoo.com)
+Enriched User Received: EnrichedUser(5,5,Bartholome Spinka IV,John West,iv_spinka_bartholome@yahoo.com)
+Enriched User Received: EnrichedUser(7,7,Afton Goyette,John West,goyette.afton@hotmail.com)
+Enriched User Received: EnrichedUser(8,8,Edwin Lind,John West,lind.edwin@yahoo.com)
+Enriched User Received: EnrichedUser(1,1,Alyson Kessler IV,John West,kessleralysoniv@gmail.com)
+Enriched User Received: EnrichedUser(5,5,Miss Cortney Labadie,John West,cortney_labadie_miss@hotmail.com)
+Enriched User Received: EnrichedUser(7,7,Giovani Wisozk,John West,giovani.wisozk@hotmail.com)
+Enriched User Received: EnrichedUser(8,8,Roel Hintz,John West,hintz.roel@yahoo.com)
+         */
+//        println(enrichedUserLog)
 
       })
     }
